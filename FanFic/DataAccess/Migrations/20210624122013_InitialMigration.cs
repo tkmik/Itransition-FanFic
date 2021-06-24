80b59ea8c8cd945586330ac87d727f4b4a1917c0
Mikita Tkach <Mikita.Tkach@itechart-group.com>
2021-06-24 15:21:37 +0300

    Create new project, change DbContext

---
+++ FanFic/DataAccess/Migrations/20210624122013_InitialMigration.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "capability",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameCapability = table.Column<int>(type: "int", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_capability", x => x.Id);
+                    table.UniqueConstraint("AK_capability_NameCapability", x => x.NameCapability);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "topic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_topic", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "fan_fic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rate = table.Column<byte>(type: "tinyint", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fan_fic", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_fan_fic_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_capability",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CapabilityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_capability", x => x.Id);
+                    table.UniqueConstraint("AK_user_capability_UserId_CapabilityId", x => new { x.UserId, x.CapabilityId });
+                    table.ForeignKey(
+                        name: "FK_user_capability_capability_CapabilityId",
+                        column: x => x.CapabilityId,
+                        principalTable: "capability",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_capability_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "comment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FanFicId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_comment_fan_fic_FanFicId",
+                        column: x => x.FanFicId,
+                        principalTable: "fan_fic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_comment_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "fan_fic_topic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FanFicId = table.Column<int>(type: "int", nullable: false),
+                    TopicId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fan_fic_topic", x => x.Id);
+                    table.UniqueConstraint("AK_fan_fic_topic_TopicId_FanFicId", x => new { x.TopicId, x.FanFicId });
+                    table.ForeignKey(
+                        name: "FK_fan_fic_topic_fan_fic_FanFicId",
+                        column: x => x.FanFicId,
+                        principalTable: "fan_fic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_fan_fic_topic_topic_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "topic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "like",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FanFicId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_like", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_like_fan_fic_FanFicId",
+                        column: x => x.FanFicId,
+                        principalTable: "fan_fic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_like_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comment_FanFicId",
+                table: "comment",
+                column: "FanFicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comment_UserId",
+                table: "comment",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_fan_fic_UserId",
+                table: "fan_fic",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_fan_fic_topic_FanFicId",
+                table: "fan_fic_topic",
+                column: "FanFicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_like_FanFicId",
+                table: "like",
+                column: "FanFicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_like_UserId",
+                table: "like",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_capability_CapabilityId",
+                table: "user_capability",
+                column: "CapabilityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "comment");
+
+            migrationBuilder.DropTable(
+                name: "fan_fic_topic");
+
+            migrationBuilder.DropTable(
+                name: "like");
+
+            migrationBuilder.DropTable(
+                name: "user_capability");
+
+            migrationBuilder.DropTable(
+                name: "topic");
+
+            migrationBuilder.DropTable(
+                name: "fan_fic");
+
+            migrationBuilder.DropTable(
+                name: "capability");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
---
+++ FanFic/WebAPI/Startup.cs
@@ -0,0 +1,74 @@
+using Autofac;
+using DataAccess.EF;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                    {
+                        string connectionString = Configuration.GetConnectionString("SQLServer");
+                        options.UseSqlServer(connectionString);
+                    })
+                    .AddSwaggerGen(options =>
+                    {
+                        options.SwaggerDoc("v1", new OpenApiInfo { Title = "FanFic", Version = "v1" });
+                    })
+                    .AddControllers()
+                    .AddNewtonsoftJson(options =>
+                    {
+                        options.SerializerSettings.Formatting = Formatting.Indented;
+                    });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options => 
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "FanFic");
+                });
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
